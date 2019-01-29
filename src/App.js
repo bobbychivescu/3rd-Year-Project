726f58a24dc1dccbfd38ec17eaa9ccf83aabfcd3
Bogdan Chivescu <bac1g16@soton.ac.uk>
2019-01-29 19:05:12 +0000

    added login with email only

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Main page, or routes, or whatever...</h1>
       </div>
     );
   }
 }
 
-export default App;
+const myTheme = {
+}
+
+const signUpConfig = {
+    hideAllDefaults: true,
+    signUpFields: [
+        {
+            label: 'Username',
+            key: 'username',
+            required: true,
+            placeholder: 'Email',
+            type: 'email',
+            displayOrder: 1
+        },
+        {
+            label: 'Password',
+            key: 'password',
+            required: true,
+            placeholder: 'Password',
+            type: 'password',
+            displayOrder: 2
+        }
+    ]
+};
+
+export default withAuthenticator(App, true, [], null, myTheme, signUpConfig);

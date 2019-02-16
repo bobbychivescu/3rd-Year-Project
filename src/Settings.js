73368c268d83e61a2c25f41fd8e332fc2ebda5a5
Bogdan Chivescu <bac1g16@soton.ac.uk>
2019-02-16 15:06:02 +0000

    completed front for settings

--- src/Settings.js
+++ src/Settings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Input } from 'reactstrap';
+import { Button, Card, Input, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { Auth, API } from 'aws-amplify';
 
 class Settings extends Component {
@@ -7,7 +7,9 @@ class Settings extends Component {
     super(props);
     this.state = {
       pass: '',
-      newPass: ''
+      newPass: '',
+      TCModal: false,
+      PPModal: false
     };
   }
 
@@ -36,6 +38,36 @@ class Settings extends Component {
     });
   };
 
+  toggleEmailNotifications = () => {
+    alert('not yet implemented');
+  };
+
+  toggleEmailAvailability = () => {
+    alert('not yet implemented');
+  };
+
+  toggleTCModal = () => {
+    this.setState(prevState => ({
+      TCModal: !prevState.TCModal
+    }));
+  };
+
+  togglePPModal = () => {
+    this.setState(prevState => ({
+      PPModal: !prevState.PPModal
+    }));
+  };
+
+  deleteAccount = async () => {
+    if (window.confirm('Are you sure you want to continue?')) {
+      API.del('3YP', '/profile');
+      const user = await Auth.currentAuthenticatedUser();
+      user.deleteUser(() => {
+        this.props.onStateChange();
+      });
+    }
+  };
+
   render() {
     return (
       <div className="settings">
@@ -58,9 +90,74 @@ class Settings extends Component {
           Change password
         </Button>
         <hr />
+        <h4>Email notifications</h4>
+        <p>
+          You currently{' '}
+          {this.props.user.emailNotifications ? (
+            <strong>receive</strong>
+          ) : (
+            <strong>DO NOT receive</strong>
+          )}{' '}
+          email notifications.
+        </p>
+        <Button onClick={this.toggleEmailNotifications} className="bg-orange">
+          Toggle notification setting
+        </Button>
+        <hr />
+        <h4>Email availability</h4>
+        <p>Choose if your contacts can see your email address.</p>
+        <p>
+          Your email is currently{' '}
+          {this.props.user.emailPublic ? (
+            <strong>public</strong>
+          ) : (
+            <strong>private</strong>
+          )}
+          .
+        </p>
+        <Button onClick={this.toggleEmailAvailability} className="bg-orange">
+          Make email{' '}
+          {this.props.user.emailPublic ? (
+            <strong>private</strong>
+          ) : (
+            <strong>public</strong>
+          )}
+        </Button>
+        <hr />
+        <h4>Legal stuff</h4>
+        <Button onClick={this.toggleTCModal} className="bg-orange">
+          Read Terms & Conditions
+        </Button>
+        <Modal centered isOpen={this.state.TCModal} toggle={this.toggleTCModal}>
+          <ModalHeader toggle={this.toggleTCModal}>
+            Terms & Conditions
+          </ModalHeader>
+          <ModalBody>yet to be composed</ModalBody>
+        </Modal>
+
+        <Button onClick={this.togglePPModal} className="bg-orange">
+          Read Privacy Policy
+        </Button>
+        <Modal centered isOpen={this.state.PPModal} toggle={this.togglePPModal}>
+          <ModalHeader toggle={this.togglePPModal}>Privacy Policy</ModalHeader>
+          <ModalBody>yet to be composed</ModalBody>
+        </Modal>
+        <hr />
         <h4>Sign out of the app</h4>
         <Button onClick={this.signOut} className="bg-orange">
-          Sing out
+          Sign out
+        </Button>
+        <hr />
+        <h4>Delete account</h4>
+        <p>
+          <strong>Warning!</strong> This action cannot be reverted.
+        </p>
+        <p>
+          This will permanently delete your account from the app, including your
+          login credentials.
+        </p>
+        <Button onClick={this.deleteAccount} color="danger">
+          Delete account
         </Button>
       </div>
     );
